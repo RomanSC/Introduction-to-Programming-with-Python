27fd77077e7df62ea67769d2bdb3e52faccb5d65
RomanSC <r.s.col988@gmail.com>
2016-09-03 16:24:43 -0400

    More fun python

--- chaos.py
+++ chaos.py
@@ -1,7 +1,8 @@
 def main():
-    print ("This program illustrates a chaotic function.")
+    print("This program illustrates a chaotic function.")
     x = input("Enter a number between 0 and 1:")
-    for i in range (10):
+    x = float(x)
+    for i in range(10):
         x = 3.9 * x * (1 - x)
         print (x)
 main()
