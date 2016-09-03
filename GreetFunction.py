e40a9b6611b5c2061fb1b71bc6d2819b0f5e20ca
RomanSC <r.s.col988@gmail.com>
2016-09-03 11:51:23 -0400

    GreetFunction.py updated for maximum trolling

---
+++ GreetFunction.py
@@ -0,0 +1,18 @@
+print ("Sup.")
+
+print ("")
+
+response = input("What's your name? ")
+print ("")
+
+print ("That's a funny name, " + response + ".")
+print ("")
+
+if response == 'Roman':
+    print  ("So like, bowling or gladiators?")
+
+if response == 'Jim':
+    print ("01010011 01101100 01101001 01101101 00100000 01001010 01101001 01101101 00101110")
+
+if response == 'Jose':
+    print ("")
