6ad57b506f64c83db688b0aa4b119d42866de1c3
Roman S. Collins <r.s.col988@gmail.com>
2016-09-03 13:51:10 -0400

    Bunch of Python apps

---
+++ Print Two Plus Three.py
@@ -0,0 +1,20 @@
+def add (x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x /y
+
+print ("I would you like to:")
+print ("1.  Add")
+print ("2.  Subtract")
+print ("3.  Multiply")
+print ("4.  Divide")
+
+choice = input ("Enter choice (1/2/3/4):")
+
