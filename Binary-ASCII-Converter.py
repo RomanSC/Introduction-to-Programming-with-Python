9ce6074f48741bb4e53295ca28f899dead8a04cc
RomanSC <r.s.col988@gmail.com>
2016-09-05 10:56:01 -0400

    Renamed Binary ASCII Converter

---
+++ Binary-ASCII-Converter.py
@@ -0,0 +1,22 @@
+print("Welcome to the binary to ASCII (text) or ASCII to binary converter. Please type \"Binary\" for binary > ASCII, or \"ASCII\" for ASCII > binary.")
+print("1. Binary > ASCII")
+print("2. ASCII > Binary ")
+response = input()
+
+if response == 'Binary':
+
+    binary = input()
+
+    bytes = binary.split()
+
+    chrs = [chr(int(byte, 2)) for byte in bytes]
+
+    print("".join(chrs))
+
+if response == 'ASCII':
+	text = input()
+
+	bytes = [format(ord(chr), 'b') for chr in text]
+
+	print(" ".join(bytes))
+
